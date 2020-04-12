52b0b59406edd4840db8cb44598f1b5083535d3f
zahrayousefijamarani <z.y.j.1379@gmail.com>
2020-04-13 01:11:04 +0430

    add switch

--- app/src/main/java/edu/sharif/sharif_dev/sensors/MainActivity.java
+++ app/src/main/java/edu/sharif/sharif_dev/sensors/MainActivity.java
@@ -1,33 +1,28 @@
 package edu.sharif.sharif_dev.sensors;
 
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private SensorManager sensorManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setSensorManager();
+        setAlarmActivity();
+        setSleepActivity();
+        setShakeActivity();
 
     }
 
-    private void setSensorManager(){
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-    }
-
-    private void startShakeActivity(){
+    private void setShakeActivity(){
 
     }
 
-    private void startSleepActivity(){
+    private void setSleepActivity(){
         ImageView sleep_mode_icon = findViewById(R.id.sleep_mode_icon);
         sleep_mode_icon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void startAlarmActivity(){
+    private void setAlarmActivity(){
 
     }
 
--- app/src/main/java/edu/sharif/sharif_dev/sensors/SleepActivity.java
+++ app/src/main/java/edu/sharif/sharif_dev/sensors/SleepActivity.java
@@ -1,13 +1,60 @@
 package edu.sharif.sharif_dev.sensors;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.content.Context;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class SleepActivity extends AppCompatActivity {
-
+    private SensorManager sensorManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
+
+        setSensorManager();
+
+        final Switch turn_switch = findViewById(R.id.switch_sleep);
+        turn_switch.setText(R.string.off);
+        turn_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    onSwitch(turn_switch);
+                else
+                    offSwitch(turn_switch);
+            }
+        });
+
     }
+
+
+    private void setSensorManager(){
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    private void onSwitch(Switch turn_switch){
+        turn_switch.setText(R.string.on);
+        setEditTextAnim(R.animator.fade_in);
+
+    }
+    private void offSwitch(Switch turn_switch){
+        turn_switch.setText(R.string.off);
+        setEditTextAnim(R.animator.fade_out);
+    }
+
+    private void setEditTextAnim(int animRes){
+        EditText degree_inp = findViewById(R.id.degree_inp);
+
+        AnimatorSet animationSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, animRes);
+        animationSet.setTarget(degree_inp);
+        animationSet.start();
+    }
+
 }
