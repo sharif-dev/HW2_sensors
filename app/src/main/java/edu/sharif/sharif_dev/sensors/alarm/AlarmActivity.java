edeb6ea3c3c784e449eb4a37f164cad7cabe206e
zahrayousefijamarani <z.y.j.1379@gmail.com>
2020-04-18 12:40:39 +0430

    add alarm receiver

--- app/src/main/java/edu/sharif/sharif_dev/sensors/alarm/AlarmActivity.java
+++ app/src/main/java/edu/sharif/sharif_dev/sensors/alarm/AlarmActivity.java
@@ -1,9 +1,14 @@
 package edu.sharif.sharif_dev.sensors.alarm;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
@@ -13,12 +18,35 @@ import java.util.Objects;
 import edu.sharif.sharif_dev.sensors.R;
 
 public class AlarmActivity extends AppCompatActivity {
+    private Intent malarmIntent;
+    PendingIntent alarmIntentPending;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
 
+        setTimeEditText();
+
+        Button alarm_btn = findViewById(R.id.alarm_btn);
+        alarm_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAlarm();
+            }
+        });
+
+        Button cancel_btn = findViewById(R.id.cancel_alarm_btn);
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                offAlarm();
+            }
+        });
+
+    }
+
+    private void setTimeEditText() {
         final EditText time_editText = findViewById(R.id.time_edit_text);
         time_editText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +67,42 @@ public class AlarmActivity extends AppCompatActivity {
                 timePickerDialog.show();
             }
         });
+    }
 
+    private void setAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        EditText time_edit = findViewById(R.id.time_edit_text);
+        Editable time_edit_text = time_edit.getText();
+        if (time_edit_text != null) {
+            String[] time_slices = time_edit.getText().toString().split(":");
+
+            // set time
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time_slices[0]));
+            calendar.set(Calendar.MINUTE, Integer.parseInt(time_slices[1]));
+
+            // set intents
+            malarmIntent = new Intent(getApplicationContext(), AlarmReceiver.class);
+            alarmIntentPending = PendingIntent.getBroadcast(getApplicationContext(), 0, malarmIntent, 0);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    AlarmManager.INTERVAL_DAY, alarmIntentPending);
+
+        } else {
+            // TODO error
+        }
+    }
+
+    private void offAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+//        PendingIntent pendingIntent =
+//                PendingIntent.getService(getApplicationContext(), 0, malarmIntent, PendingIntent.FLAG_NO_CREATE);
+//        if (pendingIntent != null && alarmManager != null) {
+//            alarmManager.cancel(pendingIntent);
+//        }
+        if(alarmIntentPending != null){
+            alarmManager.cancel(alarmIntentPending);
+        }
     }
 }
---
+++ app/src/main/java/edu/sharif/sharif_dev/sensors/alarm/AlarmReceiver.java
@@ -0,0 +1,12 @@
+package edu.sharif.sharif_dev.sensors.alarm;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
